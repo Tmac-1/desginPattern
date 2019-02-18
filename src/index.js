255e546a753dac5c28a084d9d11398cb1ce92e58
任彬彬 <binbin.ren@51zouchuqu.com>
2019-02-18 19:57:44 +0800

    初次提交

---
+++ src/index.js
@@ -0,0 +1,42 @@
+class People{
+    constructor(name,age){
+      this.name = name
+      this.age = age
+    }
+    eat(){
+       alert(`${this.name} eat something`)
+    }
+    speak(){
+      alert(`My name is ${this.name}, age ${this.age}`)
+    }
+}
+
+// let zhang  = new Person('zhang','20');
+// zhang.eat()
+// zhang.speak()
+
+// let wang = new Person('wang','21')
+// wang.eat()
+// wang.speak()
+
+class Student extends People{
+  constructor(name,age,number){
+    super(name,age)
+    this.number = number
+  }
+  study(){
+    alert(`${this.name} study`)
+  }
+}
+
+let xiaoming = new Student('xiaoming','10','A1')
+xiaoming.study()
+xiaoming.eat()
+let xiaohong = new Student('xiaohong','11','A2')
+xiaohong.study()
+xiaohong.speak()
+
+
+
+
+
