49df88d4dbb5e86cbd885bf75c27e295bb64f908
任彬彬 <binbin.ren@51zouchuqu.com>
2019-02-20 19:45:21 +0800

    设计原则

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import { callbackify } from "util";
+
 // class People{
 //     constructor(name,age){
 //       this.name = name
@@ -37,35 +39,133 @@
 // xiaohong.speak()
 
 
-class Jquery{
-  constructor(selector){
-    let slice = Array.prototype.slice;
-    let dom = slice.call(document.querySelectorAll(selector));
-    let len = dom ? dom.length : 0 ;
-    for(let i=0;i<len;i++){
-      this[i] = dom[i]
-    }
-    this.length = len
-    this.selector = selector || '';
-  }
-  append(node){
-    //...
-  }
-  addClass(name){
-    //...
-  }
-  html(data){
+// class Jquery{
+//   constructor(selector){
+//     let slice = Array.prototype.slice;
+//     let dom = slice.call(document.querySelectorAll(selector));
+//     let len = dom ? dom.length : 0 ;
+//     for(let i=0;i<len;i++){
+//       this[i] = dom[i]
+//     }
+//     this.length = len
+//     this.selector = selector || '';
+//   }
+//   append(node){
+//     //...
+//   }
+//   addClass(name){
+//     //...
+//   }
+//   html(data){
 
+//   }
+// }
+
+// window.$ = function(selector){
+//   return new Jquery(selector)
+// }
+
+// let $p = $('p')
+// console.log($p);
+// console.log($p.addClass)
+
+
+// function loadImg (src){
+//   let promise = new Promise((resolve,reject)=>{
+//       let img = document.createElement('img')
+//       img.onload = ()=>{
+//         resolve(img)
+//       }
+//       img.onerror = ()=>{
+//         reject('图片加载失败')
+//       }
+//       img.src = src
+//   })
+//   return promise
+// }
+
+// let result = loadImg('https://wx.qlogo.cn/mmopen/vi_32/Q0j4TwGTfTKmB1DWZP1l7Q9NPNFNFopPTYQrG1JFyTDs0K5icVrWoZDjxjyVmnwnkuSiakxnOVNzXJZtaU0AsnlA/132')
+// result.then((img)=>{
+//   alert(`width: ${img.width}`)
+//   return img
+// }).then((img)=>{
+//   alert(`height: ${img.height}`)
+// }).catch((error)=>{
+//   alert(error)
+// })
+
+class Car {
+  constructor(number,name){
+    this.number = number;
+    this.name = name
   }
 }
 
-window.$ = function(selector){
-  return new Jquery(selector)
+class Kuaiche extends Car{
+  constructor(number,name){
+    super(number,name)
+    this.price = 1;
+  }
 }
 
-let $p = $('p')
-console.log($p);
-console.log($p.addClass)
+class Zhuanche extends Car{
+  constructor(number,name){
+    super(number,name)
+    this.price = 2;
+  }
+}
+
+class Trip {
+  constructor(car){
+      this.car = car
+  }
+  start(){
+      console.log(`车名 ${this.car.name} 车牌号 ${this.car.number}`)
+  }
+  end(){
+       console.log(`行程结束${this.car.price*5}元`)
+  }
+}
+
+let car = new Kuaiche('100','保时捷');
+let trip = new Trip(car);
+trip.start();
+trip.end()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
