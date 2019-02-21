3485251ec7119e279f2ceae023c9eae35d34a466
任彬彬 <binbin.ren@51zouchuqu.com>
2019-02-21 19:53:16 +0800

    添加题目

--- src/index.js
+++ src/index.js
@@ -94,43 +94,172 @@ import { callbackify } from "util";
 //   alert(error)
 // })
 
-class Car {
-  constructor(number,name){
-    this.number = number;
-    this.name = name
+// class Car {
+//   constructor(number,name){
+//     this.number = number;
+//     this.name = name
+//   }
+// }
+
+// class Kuaiche extends Car{
+//   constructor(number,name){
+//     super(number,name)
+//     this.price = 1;
+//   }
+// }
+
+// class Zhuanche extends Car{
+//   constructor(number,name){
+//     super(number,name)
+//     this.price = 2;
+//   }
+// }
+
+// class Trip {
+//   constructor(car){
+//       this.car = car
+//   }
+//   start(){
+//       console.log(`车名 ${this.car.name} 车牌号 ${this.car.number}`)
+//   }
+//   end(){
+//        console.log(`行程结束${this.car.price*5}元`)
+//   }
+// }
+
+// let car = new Kuaiche('100','保时捷');
+// let trip = new Trip(car);
+// trip.start();
+// trip.end()
+
+class Car{
+  constructor(num){
+    this.num = num
+  }
+}
+class Camera {
+  shot(car){
+    return {
+      num:car.num,
+      inTime:Date.now()
+    }
+  }
+}
+class Screen {
+  show(car,inTime){
+    console.log('车牌号',car.num)
+    console.log('停车时间',Date.now() - inTime)
+  }
+}
+class Park{
+   constructor(floors){
+     this.floors = floors || []
+     this.camera = new Camera()
+     this.screen = new Screen()
+     this.carList = {} // 存储摄像头返回的车辆信息
+   }
+   in(car){
+      const info = this.camera.shot(car)
+      const i = parseInt( Math.random() * 100 % 100 )
+      const place = this.floors[0].places[i]
+      place.in()
+      info.place = place
+      this.carList[car.num] = info;
+   }
+   out(car){
+      const info = this.carList[car.num]
+      const place = info.place
+      place.out()
+      this.screen.show(car,info.inTime)
+      delete this.carList[car.num]
+   }
+   emptyNum(){
+    return this.floors.map(floor=>{
+       return `${floor.index} 层还有 ${floor.emptyPlaceNum()} 个空闲车位`
+     }).join('\n')
+   }
+}
+
+class Floor{
+  constructor(index,places){
+    this.index = index
+    this.places = places || []
+  }
+  emptyPlaceNum(){
+     let num = 0;
+     this.places.forEach(p=>{
+       if(p.empty){
+         num = num + 1;
+       }
+     })
+     return num;
+  }
+}
+class Place{
+  constructor(){
+    this.empty = true
+  }
+  in(){
+    this.empty = false
+  }
+  out(){
+    this.empty = true
   }
 }
 
-class Kuaiche extends Car{
-  constructor(number,name){
-    super(number,name)
-    this.price = 1;
-  }
-}
 
-class Zhuanche extends Car{
-  constructor(number,name){
-    super(number,name)
-    this.price = 2;
+const floors = []
+for(let i=0;i<3;i++){
+  const places = [];
+  for(let j=0;j<100;j++){
+    places[j] = new Place()
   }
+  floors[i] =  new Floor(i+1,places)
 }
+const park = new Park(floors)
+const car1 = new Car(100)
+const car2 = new Car(200)
+const car3 = new Car(300)
 
-class Trip {
-  constructor(car){
-      this.car = car
-  }
-  start(){
-      console.log(`车名 ${this.car.name} 车牌号 ${this.car.number}`)
-  }
-  end(){
-       console.log(`行程结束${this.car.price*5}元`)
-  }
-}
+console.log('第一辆车进入')
+console.log(park.emptyNum())
+park.in(car1)
+console.log('第二辆车进入')
+console.log(park.emptyNum())
+park.in(car2)
+console.log('第一辆车离开')
+park.out(car1)
+console.log('第二辆车离开')
+park.out(car2)
 
-let car = new Kuaiche('100','保时捷');
-let trip = new Trip(car);
-trip.start();
-trip.end()
+console.log('第三辆车进入')
+console.log(park.emptyNum())
+park.in(car3)
+console.log('第三辆车离开')
+park.out(car3)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
