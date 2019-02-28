f785fd369bcda11ef5f6300c4901751ce7d035d7
任彬彬 <binbin.ren@51zouchuqu.com>
2019-02-28 19:39:19 +0800

    添加单例模式

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-import { callbackify } from "util";
 
 // class People{
 //     constructor(name,age){
@@ -132,111 +131,195 @@ import { callbackify } from "util";
 // trip.start();
 // trip.end()
 
-class Car{
-  constructor(num){
-    this.num = num
+// class Car{
+//   constructor(num){
+//     this.num = num
+//   }
+// }
+// class Camera {
+//   shot(car){
+//     return {
+//       num:car.num,
+//       inTime:Date.now()
+//     }
+//   }
+// }
+// class Screen {
+//   show(car,inTime){
+//     console.log('车牌号',car.num)
+//     console.log('停车时间',Date.now() - inTime)
+//   }
+// }
+// class Park{
+//    constructor(floors){
+//      this.floors = floors || []
+//      this.camera = new Camera()
+//      this.screen = new Screen()
+//      this.carList = {} // 存储摄像头返回的车辆信息
+//    }
+//    in(car){
+//       const info = this.camera.shot(car)
+//       const i = parseInt( Math.random() * 100 % 100 )
+//       const place = this.floors[0].places[i]
+//       place.in()
+//       info.place = place
+//       this.carList[car.num] = info;
+//    }
+//    out(car){
+//       const info = this.carList[car.num]
+//       const place = info.place
+//       place.out()
+//       this.screen.show(car,info.inTime)
+//       delete this.carList[car.num]
+//    }
+//    emptyNum(){
+//     return this.floors.map(floor=>{
+//        return `${floor.index} 层还有 ${floor.emptyPlaceNum()} 个空闲车位`
+//      }).join('\n')
+//    }
+// }
+
+// class Floor{
+//   constructor(index,places){
+//     this.index = index
+//     this.places = places || []
+//   }
+//   emptyPlaceNum(){
+//      let num = 0;
+//      this.places.forEach(p=>{
+//        if(p.empty){
+//          num = num + 1;
+//        }
+//      })
+//      return num;
+//   }
+// }
+// class Place{
+//   constructor(){
+//     this.empty = true
+//   }
+//   in(){
+//     this.empty = false
+//   }
+//   out(){
+//     this.empty = true
+//   }
+// }
+
+
+// const floors = []
+// for(let i=0;i<3;i++){
+//   const places = [];
+//   for(let j=0;j<100;j++){
+//     places[j] = new Place()
+//   }
+//   floors[i] =  new Floor(i+1,places)
+// }
+// const park = new Park(floors)
+// const car1 = new Car(100)
+// const car2 = new Car(200)
+// const car3 = new Car(300)
+
+// console.log('第一辆车进入')
+// console.log(park.emptyNum())
+// park.in(car1)
+// console.log('第二辆车进入')
+// console.log(park.emptyNum())
+// park.in(car2)
+// console.log('第一辆车离开')
+// park.out(car1)
+// console.log('第二辆车离开')
+// park.out(car2)
+
+// console.log('第三辆车进入')
+// console.log(park.emptyNum())
+// park.in(car3)
+// console.log('第三辆车离开')
+// park.out(car3)
+
+// 工厂模式
+// class Product{
+//   constructor(name){
+//     this.name = name
+//   }
+//   init(){
+//     alert('init')
+//   }
+//   fun1(){
+//     alert('fun1')
+//   }
+//   fun2(){
+//     alert('fun2')
+//   }
+// }
+
+// class Creator{
+//   create(name){
+//     return new Product(name)
+//   }
+// }
+
+// let creator = new Creator();
+// let p = creator.create('p1')
+// p.init()
+// p.fun1()
+
+// 单例模式
+class SingleInstance{
+  login(){
+    console.log('...login')
   }
 }
-class Camera {
-  shot(car){
-    return {
-      num:car.num,
-      inTime:Date.now()
+
+SingleInstance.getInstance = function(){
+  let instance;
+  return function(){
+    if(!instance){
+      instance = new SingleInstance()
     }
+    return instance;
   }
-}
-class Screen {
-  show(car,inTime){
-    console.log('车牌号',car.num)
-    console.log('停车时间',Date.now() - inTime)
-  }
-}
-class Park{
-   constructor(floors){
-     this.floors = floors || []
-     this.camera = new Camera()
-     this.screen = new Screen()
-     this.carList = {} // 存储摄像头返回的车辆信息
-   }
-   in(car){
-      const info = this.camera.shot(car)
-      const i = parseInt( Math.random() * 100 % 100 )
-      const place = this.floors[0].places[i]
-      place.in()
-      info.place = place
-      this.carList[car.num] = info;
-   }
-   out(car){
-      const info = this.carList[car.num]
-      const place = info.place
-      place.out()
-      this.screen.show(car,info.inTime)
-      delete this.carList[car.num]
-   }
-   emptyNum(){
-    return this.floors.map(floor=>{
-       return `${floor.index} 层还有 ${floor.emptyPlaceNum()} 个空闲车位`
-     }).join('\n')
-   }
-}
+}()
 
-class Floor{
-  constructor(index,places){
-    this.index = index
-    this.places = places || []
-  }
-  emptyPlaceNum(){
-     let num = 0;
-     this.places.forEach(p=>{
-       if(p.empty){
-         num = num + 1;
-       }
-     })
-     return num;
-  }
-}
-class Place{
-  constructor(){
-    this.empty = true
-  }
-  in(){
-    this.empty = false
-  }
-  out(){
-    this.empty = true
-  }
-}
+let In1 = SingleInstance.getInstance()
+let In2 = SingleInstance.getInstance()
+In1.login()
+In2.login()
+console.log('in1===in2',In1 === In2)
 
 
-const floors = []
-for(let i=0;i<3;i++){
-  const places = [];
-  for(let j=0;j<100;j++){
-    places[j] = new Place()
-  }
-  floors[i] =  new Floor(i+1,places)
-}
-const park = new Park(floors)
-const car1 = new Car(100)
-const car2 = new Car(200)
-const car3 = new Car(300)
 
-console.log('第一辆车进入')
-console.log(park.emptyNum())
-park.in(car1)
-console.log('第二辆车进入')
-console.log(park.emptyNum())
-park.in(car2)
-console.log('第一辆车离开')
-park.out(car1)
-console.log('第二辆车离开')
-park.out(car2)
 
-console.log('第三辆车进入')
-console.log(park.emptyNum())
-park.in(car3)
-console.log('第三辆车离开')
-park.out(car3)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
