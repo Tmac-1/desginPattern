b86468e8d2434529ed6ab1324289556f07bf9329
任彬彬 <binbin.ren@51zouchuqu.com>
2019-03-07 19:42:32 +0800

    观察者模式

--- src/index.js
+++ src/index.js
@@ -332,29 +332,77 @@
 // d1.draw()
 
 // 代理模式
-class RealImg {
-  constructor(filename){
-    this.filename = filename;
-    this.loadImgFromDisk()
-  }
-  display(){
-    console.log('diplay...',this.filename)
-  }
-  loadImgFromDisk(){
-    console.log('loading...',this.filename)
-  }
+// class RealImg {
+//   constructor(filename){
+//     this.filename = filename;
+//     this.loadImgFromDisk()
+//   }
+//   display(){
+//     console.log('diplay...',this.filename)
+//   }
+//   loadImgFromDisk(){
+//     console.log('loading...',this.filename)
+//   }
+// }
+// class ProxyImg{
+//   constructor(filename){
+//     this.filename = new RealImg(filename)
+//   }
+//   display(){
+//     this.filename.display()
+//   }
+// }
+
+// let proxyImg = new ProxyImg('1.png');
+// proxyImg.display()
+
+// 观察者模式
+/**主题，保存状态，状态变化之后触发所有观察者对象 */ 
+class Subject{
+   constructor(){
+     this.state = 0;
+     this.observers = [];
+   }
+   getState(){
+     return this.state;
+   }
+   setState(state){
+     this.state = state;
+     this.notifyAllObservers()
+   }
+   notifyAllObservers(){
+     this.observers.forEach(observer=>{
+       observer.update()
+     })
+   }
+   attach(observer){
+     this.observers.push(observer)
+   }
 }
-class ProxyImg{
-  constructor(filename){
-    this.filename = new RealImg(filename)
+
+class Observer {
+  constructor(name,subject){
+    this.name = name;
+    this.subject = subject;
+    this.subject.attach(this)
   }
-  display(){
-    this.filename.display()
+  update(){
+    console.log(`${this.name} update, state: ${this.subject.getState()}`)
   }
 }
 
-let proxyImg = new ProxyImg('1.png');
-proxyImg.display()
+let s1 = new Subject();
+let o1 = new Observer('o1',s1);
+let o2 = new Observer('o2',s1);
+let o3 = new Observer('o3',s1);
+s1.setState(1)
+
+
+
+
+
+
+
 
 
 
