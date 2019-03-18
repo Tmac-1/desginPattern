cdd37f33d2cede3ea8cedd9cfef2c7c665788ee7
任彬彬 <binbin.ren@51zouchuqu.com>
2019-03-18 19:58:07 +0800

    添加折扣

--- src/demo/ShoppingCart/shoppingCart.js
+++ src/demo/ShoppingCart/shoppingCart.js
@@ -1,8 +1,30 @@
+import $ from 'jquery';
+import getCart from './GetCart';
+
 export default class ShoppingCart {
     constructor(app){
-
+       this.app = app;
+       this.$el = $('<div>').css({
+           'padding-bottom':'10px',
+           'border-bottom':'1px solid #ccc'
+       })
+       this.cart = getCart()
+    }
+    initBtn(){
+        let $btn = $('<button>购物车</button>')
+        $btn.click(()=>{
+            this.showCart()
+        })
+        this.$el.append($btn)
+    }
+    showCart(){
+        alert(this.cart.getList())
     }
     init(){
-        
+       this.initBtn();
+       this.render()
+    }
+    render() {
+        this.app.$el.append(this.$el)
     }
 }
